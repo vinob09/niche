7f7f185282bb76afb155ba6bdfb828a6141e6c3a
Chanary Wilson <137831268+vinob09@users.noreply.github.com>
2024-07-30 16:17:27 -0700

    Added associations to table models (#5)

--- app/models/favorite.py
+++ app/models/favorite.py
@@ -5,6 +5,21 @@ from .user import User
 class Favorite(db.Model):
     __tablename__ = 'favorites'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('Product.id'), nullable=False)
+
+    # many to one with User
+    user = db.relationship("User", back_populates="favorites")
+    # many to one with Product
+    product = db.relationship("Product", back_populates="favorites")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.user_id,
+            'productId': self.product_id
+        }
--- app/models/pastOrder.py
+++ app/models/pastOrder.py
@@ -1,11 +1,29 @@
 from .db import db, environment, SCHEMA
 from .user import User
+from .orderItem import OrderItem
 
 class PastOrder(db.Model):
     __tablename__ = 'past_orders'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     purchaser_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
     total = db.Column(db.Float, nullable=False)
     purchase_date = db.Column(db.Date, nullable=False)
     has_reviewed = db.Column(db.Boolean, nullable=False)
+
+    # many to one with User
+    user = db.relationship("User", back_populates="orders")
+    # one to many with OrderItem
+    order_items = db.relationship("OrderItem", back_populates="order")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.user_id,
+            'productId': self.product_id,
+            'review': self.review,
+            'starRating': self.star_rating
+        }
