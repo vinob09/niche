6cdb4288c34c38201d4def74769b3044bcddde72
Julia Duncan <juliataylorduncan@gmail.com>
2024-08-01 14:08:31 -0700

    Products GET backend routes

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
-from .api.products.get import product_routes
+from .api.products.get import product_routes, category_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -30,6 +30,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(product_routes, url_prefix='/api/products')
+app.register_blueprint(category_routes, url_prefix='/api/categories')
 db.init_app(app)
 Migrate(app, db)
 
--- app/api/products/get.py
+++ app/api/products/get.py
@@ -1,12 +1,64 @@
 from flask import Blueprint, render_template
-#IMPORT FORMS
-from app.models import Product
+from flask_login import current_user, login_required
+from app.models import Product, ProductImage, Review, Favorite, Category, db
 
 product_routes = Blueprint("products", __name__)
+category_routes = Blueprint("categories", __name__)
 
-'''GET all products'''
+'''GET all Products'''
 @product_routes.route("/products", methods=['GET'])
 def get_all_products():
     products = Product.query.all()
-    '''LANDING PAGE TO TAKE IN products'''
-    return render_template('/products', products=products)
+    return render_template('productsComponent', products=products)
+
+'''GET a Product by product_id'''
+@product_routes.route("/products/<int:product_id>", methods=['GET'])
+def get_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    product_images = ProductImage.query.filter_by(product_id=product_id).all()
+    reviews = Review.query.filter_by(product_id=product_id).all()
+
+    product_data = product.to_dict()
+    product_data['images'] = [image.to_dict() for image in product_images]
+    product_data['reviews'] = [review.to_dict() for review in reviews]
+
+
+    is_favorited = False
+    if current_user.is_authenticated:
+        favorite = Favorite.query.filter_by(user_id=current_user.id,product_id=product_id).first()
+        if favorite:
+            is_favorited = True
+    
+    product_data['is_favorited'] = is_favorited
+
+    return render_template('/productdetail.html', product=product_data)
+
+'''GET all Products Owned by the Current User'''
+@product_routes.route("/prducts/current", methods=['GET'])
+@login_required
+def get_curr_user_products():
+
+    products = Product.query.filter_by(seller_id=current_user.id).all()
+    products_data = [product.to_dict() for product in products]
+
+    return render_template('/categoryComponent', products_data=products_data)
+
+'''CATEGORIES.. own routes?'''
+
+'''GET all Products by Category'''
+@product_routes.route("/products/categories/<int:category_id>", methods=['GET'])
+def get_products_by_category(category_id):
+    category = Category.query.get_or_404(category_id)
+
+    products = Product.query.filter_by(category_id=category_id).all()
+    products_data = [product.to_dict() for product in products]
+
+    return render_template('/categorydetail.html', products_data=products_data)
+
+'''GET all Categories'''
+@category_routes.route("/categories", methods=['GET'])
+def get_categories():
+    categories = Category.query.all()
+    categories_data = [category.to_dict() for category in categories]
+
+    return render_template('categoryMenuComp', categories_data=categories_data)
