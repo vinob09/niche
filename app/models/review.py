f74998cc47fc76585a7b59941e8f6b0c3467d011
Chanary Wilson <137831268+vinob09@users.noreply.github.com>
2024-08-01 17:51:02 -0700

    Fixed bug (#22)

--- app/models/review.py
+++ app/models/review.py
@@ -6,7 +6,7 @@ class Review(db.Model):
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('products.id')), nullable=False)
     review = db.Column(db.String(1000), nullable=False)
