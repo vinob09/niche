d199e3bee970f51dbff9a8013041512b2124f897
Ruben Polanco <andr3z.93@gmail.com>
2024-08-05 14:45:04 -0700

    finished the put route for cart-item

--- app/api/cart_items/post.py
+++ app/api/cart_items/post.py
@@ -7,18 +7,25 @@ cart_add_items = Blueprint('cart_items_post', __name__)
 
 '''GET all cart items'''
 
-@cart_add_items.route("/<int:product_id>", methods=['POST'])
+@cart_add_items.route("/<int:product_id>", methods=['POST', 'PUT'])
 @login_required
 def add_to_cart(product_id):
-    cart_item = CartItem(
+    check_cart = CartItem.query.filter(CartItem.user_id==current_user.id, CartItem.product_id==product_id).all()
+    if check_cart:
+        r = request.get_json()
+        check_cart[0].quantity = r['quantity']
+        db.session.commit()
+        return jsonify(check_cart[0].to_dict())
+    else:
+        cart_item = CartItem(
         user_id = current_user.id,
         product_id = product_id,
         quantity=1
-    )
+        )
 
-    db.session.add(cart_item)
-    db.session.commit()
+        db.session.add(cart_item)
+        db.session.commit()
 
-    return jsonify({
-        'cart_Id': cart_item.id
-    }), 201
+        return jsonify({
+            'cart_Id': cart_item.id
+        }), 200
