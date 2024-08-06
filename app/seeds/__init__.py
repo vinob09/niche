856c9087acf095544e582e449704d4509f544066
Chanary Wilson <137831268+vinob09@users.noreply.github.com>
2024-08-05 17:16:44 -0700

    Completed Product Images seeder (#34)

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -7,6 +7,7 @@ from .favorites import seed_favorite, undo_favorite
 from .orderItems import seed_order_items, undo_order_items
 from .pastOrders import seed_past_orders, undo_past_orders
 from .cartItems import seed_cart_items, undo_cart_items
+from .productImages import seed_images, undo_images
 
 from app.models.db import db, environment, SCHEMA
 
@@ -23,6 +24,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_images()
         undo_cart_items()
         undo_order_items()
         undo_past_orders()
@@ -39,12 +41,14 @@ def seed():
     seed_past_orders()
     seed_order_items()
     seed_cart_items()
+    seed_images()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+        undo_images()
         undo_cart_items()
         undo_order_items()
         undo_past_orders()
---
+++ app/seeds/productImages.py
@@ -0,0 +1,129 @@
+from app.models import db, ProductImage, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+# Adding product_images seeder data
+def seed_images():
+    board1 = ProductImage(
+        product_id=1,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/board1.jpg"
+    )
+    board2 = ProductImage(
+        product_id=1,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/board2.jpg"
+    )
+    portrait1 = ProductImage(
+        product_id=2,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/portrait1.jpg"
+    )
+    portrait2 = ProductImage(
+        product_id=2,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/portrait2.jpg"
+    )
+    deskmat1 = ProductImage(
+        product_id=3,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/deskmat1.jpg"
+    )
+    deskmat2 = ProductImage(
+        product_id=3,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/deskmat2.jpg"
+    )
+    charger1 = ProductImage(
+        product_id=4,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/charger1.jpg"
+    )
+    charger2 = ProductImage(
+        product_id=4,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/charger2.jpg"
+    )
+    cooler1 = ProductImage(
+        product_id=5,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/cooler1.jpg"
+    )
+    cooler2 = ProductImage(
+        product_id=5,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/cooler2.jpg"
+    )
+    rackets1 = ProductImage(
+        product_id=6,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/rackets1.jpg"
+    )
+    rackets2 = ProductImage(
+        product_id=6,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/rackets2.jpg"
+    )
+    car1 = ProductImage(
+        product_id=7,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/car1.jpg"
+    )
+    car2 = ProductImage(
+        product_id=7,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/car2.jpg"
+    )
+    psfive1 = ProductImage(
+        product_id=8,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/psfive1.jpg"
+    )
+    psfive2 = ProductImage(
+        product_id=8,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/psfive2.jpg"
+    )
+    exhaust1 = ProductImage(
+        product_id=9,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/exhaust1.jpg"
+    )
+    exhaust2 = ProductImage(
+        product_id=9,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/exhaust2.jpg"
+    )
+    kw1 = ProductImage(
+        product_id=10,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/kw1.jpg"
+    )
+    kw2 = ProductImage(
+        product_id=10,
+        url="https://chansbucket.s3.us-east-2.amazonaws.com/Products-Images/Products-Images/kw2.jpg"
+    )
+
+    all_images = [
+        board1,
+        board2,
+        portrait1,
+        portrait2,
+        deskmat1,
+        deskmat2,
+        charger1,
+        charger2,
+        cooler1,
+        cooler2,
+        rackets1,
+        rackets2,
+        car1,
+        car2,
+        psfive1,
+        psfive2,
+        exhaust1,
+        exhaust2,
+        kw1,
+        kw2
+    ]
+
+    add_all_images = [db.session.add(image) for image in all_images]
+    print("All images added")
+
+    db.session.commit(add_all_images)
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_images():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.product_images RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM product_images"))
+
+    db.session.commit()
