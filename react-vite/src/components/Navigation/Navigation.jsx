ef4ee8f382b5feb04ec65ab9bd2d069d65c3be0c
Julia Duncan <juliataylorduncan@gmail.com>
2024-08-07 16:59:23 -0700

    nav edit 1

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -4,32 +4,35 @@ import "./Navigation.css";
 
 function Navigation({hasLoggedIn}) {
   return (
-    <ul className="navbar">
-      <li>
-        <NavLink to="/" className={"home-link"}>niche</NavLink>
-      </li>
-      <li className="categories-button">
-        <NavLink to="/categories">categories</NavLink>
-      </li>
-      <li className="search-bar">
-        <input type="text" placeholder="Search"/>
-      </li>
-      {hasLoggedIn ? (
-        <>
-        <li className="favorites-button">
-          <NavLink to="/favorites">favorites</NavLink>
+    <div className="navbar">
+      <div className="gradient-overlay"></div>
+    <ul>
+        <li>
+          <NavLink to="/" className={"home-link"}>niche</NavLink>
         </li>
-        <li className="products-button">
-          <NavLink to="/past-orders">products</NavLink>
+        <li className="categories-button">
+          <NavLink to="/categories">categories</NavLink>
         </li>
-        </>
-      ): null }
+        <li className="search-bar">
+          <input type="text" placeholder="Search" />
+        </li>
+        {hasLoggedIn ? (
+          <>
+            <li className="favorites-button">
+              <NavLink to="/favorites">favorites</NavLink>
+            </li>
+            <li className="products-button">
+              <NavLink to="/past-orders">products</NavLink>
+            </li>
+          </>
+        ) : null}
 
-      <li className='user-cart'>
-        <ProfileButton />
-        <NavLink to="/shopping-cart" className="shopping-cart">cart</NavLink>
-      </li>
-    </ul>
+        <li className='user-cart'>
+          <ProfileButton />
+          <NavLink to="/shopping-cart" className="shopping-cart">cart</NavLink>
+        </li>
+      </ul>
+    </div>
   );
 }
 
