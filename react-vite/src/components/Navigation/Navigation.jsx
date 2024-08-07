75ee37be517e4ece62406a0aa1151724467625d6
Julia Duncan <juliataylorduncan@gmail.com>
2024-08-07 16:28:45 -0700

    navBar update and styling

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -2,15 +2,32 @@ import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
-function Navigation() {
+function Navigation({hasLoggedIn}) {
   return (
     <ul className="navbar">
       <li>
         <NavLink to="/" className={"home-link"}>niche</NavLink>
       </li>
+      <li className="categories-button">
+        <NavLink to="/categories">categories</NavLink>
+      </li>
+      <li className="search-bar">
+        <input type="text" placeholder="Search"/>
+      </li>
+      {hasLoggedIn ? (
+        <>
+        <li className="favorites-button">
+          <NavLink to="/favorites">favorites</NavLink>
+        </li>
+        <li className="products-button">
+          <NavLink to="/past-orders">products</NavLink>
+        </li>
+        </>
+      ): null }
 
-      <li>
+      <li className='user-cart'>
         <ProfileButton />
+        <NavLink to="/shopping-cart" className="shopping-cart">cart</NavLink>
       </li>
     </ul>
   );
