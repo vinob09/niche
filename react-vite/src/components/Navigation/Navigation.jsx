783d61b103477738bfcc9c8b6c34f815e7410e4f
Julia Duncan <124819703+JuliaTDunc@users.noreply.github.com>
2024-08-07 17:24:40 -0700

    Added normalize.css for all pages. Edited the navigation bar elements & styling (#46)
    * normalize.css + src/assets created
    
    * navBar update and styling
    
    * nav edit 1
    
    * tm symbol

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -2,17 +2,37 @@ import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
-function Navigation() {
+function Navigation({hasLoggedIn}) {
   return (
-    <ul className="navbar">
-      <li>
-        <NavLink to="/" className={"home-link"}>niche</NavLink>
-      </li>
+    <div className="navbar">
+      <div className="gradient-overlay"></div>
+    <ul>
+        <li>
+          <NavLink to="/" className={"home-link"}>niche™</NavLink>
+        </li>
+        <li className="categories-button">
+          <NavLink to="/categories">categories</NavLink>
+        </li>
+        <li className="search-bar">
+          <input type="text" placeholder="Search" />
+        </li>
+        {hasLoggedIn ? (
+          <>
+            <li className="favorites-button">
+              <NavLink to="/favorites">favorites</NavLink>
+            </li>
+            <li className="products-button">
+              <NavLink to="/past-orders">products</NavLink>
+            </li>
+          </>
+        ) : null}
 
-      <li>
-        <ProfileButton />
-      </li>
-    </ul>
+        <li className='user-cart'>
+          <ProfileButton />
+          <NavLink to="/shopping-cart" className="shopping-cart">cart</NavLink>
+        </li>
+      </ul>
+    </div>
   );
 }
 
