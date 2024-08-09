ec66cccb025cd85920f56c261bc09e420ba72205
Julia Duncan <juliataylorduncan@gmail.com>
2024-08-09 12:24:38 -0700

    Product form

--- react-vite/src/components/NewProductFormPage/NewProductFormPage.jsx
+++ react-vite/src/components/NewProductFormPage/NewProductFormPage.jsx
@@ -1,10 +1,94 @@
-import './NewProductFormPage.css'
+import './NewProductFormPage.css';
+import React, {useEffect, useState} from 'react';
+import { fetchCreateProduct } from '../../redux/products';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 function NewProductFormPage () {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [categoryId, setCategoryId] = useState('');
+    const [categories, setCategories] = useState([]);
+    const [errors, setErrors] = useState([]);
+    const [isLoaded, setIsLoaded] = useState('false');
+
+    useEffect(() => {
+        const fetchCategories = async() => {
+            const res = await fetch('api/categories');
+            const data = await res.json();
+            setCategories(data.categories);
+        };
+        fetchCategories();
+    }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+
+        const newProduct = {
+            name,
+            description,
+            price: parseFloat(price),
+            category_id: categoryId
+        };
+
+        try {
+            const createdProduct = await dispatch(fetchCreateProduct(newProduct));
+            navigate(`/products/${createdProduct.id}`);
+
+        } catch (error) {
+            const errorObj = await error.json();
+            setErrors(errorObj.errors)
+        }
+    }
+
     return (
-        <div className='new-product-form-page'>
-            <h1>Create New Product Form Page</h1>
-        </div>
+        <form onSubmit={handleSubmit} className = 'product-form'>
+            <ul>
+                {errors.map((err, i) => <li key={i}>{err}</li>)}
+            </ul>
+            <div>
+                <label htmlFor="name">product name...</label>
+                <input
+                id='name' type='text' value={name}
+                onChange={(e) => setName(e.target.value)}
+                required maxLength='50
+                '/>
+            </div>
+            <div>
+                <label htmlFor='description'>product description...</label>
+                <input
+                id='description' value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required maxLength='1000'
+                />
+            </div>
+            <div>
+                <label htmlFor='price'>set a price</label>
+                <input id='price' type='number' value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                required min='0' step='0.01'
+                />
+            </div>
+            <div>
+                <label htmlFor='category'>select a category..</label>
+                <select id='category' value={categoryId}
+                onChange={(e) => setCategoryId(e.target.value)}
+                required>
+                   <option value=''>select a category for your product</option>
+                   {categories.map(category => (
+                    <option key={category.id} value={category.id}>
+                        {category.name}
+                    </option>
+                   ))}
+                </select>
+            </div>
+            <button type='submit'>create product</button>
+        </form>
     )
 }
 
