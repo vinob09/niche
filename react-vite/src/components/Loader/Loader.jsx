8f49825fc2465b9501c230275bcae185add917ad
Julia Duncan <juliataylorduncan@gmail.com>
2024-08-09 17:01:09 -0700

    newLoader

--- react-vite/src/components/Loader/Loader.jsx
+++ react-vite/src/components/Loader/Loader.jsx
@@ -3,13 +3,22 @@ import './Loader.css'
 
 function Loader() {
     return (
-        <section className="loader">
+        /*<section className="loader">
             <div style={{ '--i': 0 }} className="slider"></div>
             <div style={{ '--i': 1 }} className="slider"></div>
             <div style={{ '--i': 2 }} className="slider"></div>
             <div style={{ '--i': 3 }} className="slider"></div>
             <div style={{ '--i': 4 }} className="slider"></div>
+        </section>*/
+        /* From Uiverse.io by adamgiebl */
+        <section class="dots-container">
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
         </section>
+
     );
 }
 
