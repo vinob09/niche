36ac0fc48b9f6502b8a829e886e367772d66998e
Chanary Wilson <137831268+vinob09@users.noreply.github.com>
2024-08-12 10:58:58 -0700

    Updates to Landing Page, Best Sellers Bar (#59)
    * Added 1 more Product seeder data
    
    * Updated Best Sellers Bar
    
    * Notes added about more sections in LandingPage

--- react-vite/src/components/BestSellersBar/BestSellersBar.jsx
+++ react-vite/src/components/BestSellersBar/BestSellersBar.jsx
@@ -5,12 +5,14 @@ function BestSellersBar({ products }) {
     const bestSellers = products.slice(4, 10);
 
     return (
-        <div className='best-sellers-bar'>
-            <h2>Best Sellers</h2>
-            <div className='best-sellers-content'>
-                {bestSellers.map(product => (
-                    <ProductTiles key={product.id} product={product} />
-                ))}
+        <div className='best-sellers-wrapper'>
+            <h2 className='best-sellers-title'>Best Sellers</h2>
+            <div className='best-sellers-bar'>
+                <div className='best-sellers-content'>
+                    {bestSellers.map(product => (
+                        <ProductTiles key={product.id} product={product} />
+                    ))}
+                </div>
             </div>
         </div>
     )
