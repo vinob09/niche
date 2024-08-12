f5e07c8f2bfe6929751a926ead021ed899f70fa8
Ruben Polanco <andr3z.93@gmail.com>
2024-08-12 13:38:49 -0700

    created v1 of the cartDetails page for testing

--- react-vite/src/components/CartDetailsPage/CartDetailsPage.jsx
+++ react-vite/src/components/CartDetailsPage/CartDetailsPage.jsx
@@ -1,11 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { fetchCartItems } from '../../redux/orders';
+import Loader from '../Loader/Loader';
 import './CartDetailsPage.css';
 
 function CartDetailsPage () {
-    return (
+    const dispatch = useDispatch();
+    const [isLoaded, setIsLoaded] = useState(false);
+    const cartItems = useSelector(state => Object.values(state.orders.cartItems))
+    const currentUser = useSelector(state => state.user.id)
+
+    useEffect(() => {
+        dispatch(fetchCartItems(currentUser))
+        .then(() => {
+            setIsLoaded(true)
+        })
+    }, [dispatch, currentUser])
+
+    return isLoaded ? (
         <div className='cart-details-page'>
             <h1>Cart Details Page</h1>
+            <div className='cart-list'>
+                <ul>
+                    {cartItems.map(item => {
+                        return <li key={item.id}>cartItems.product</li>
+                    })}
+                </ul>
+            </div>
         </div>
-    )
+    ) : <Loader />
 }
 
 
