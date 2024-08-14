d2b6359ee4e851e6a4c4cce1358345387d83b495
Chanary Wilson <chanarykw@gmail.com>
2024-08-14 10:59:44 -0700

    Fixed normalize.css

--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -5,8 +5,9 @@ import { RouterProvider } from "react-router-dom";
 import configureStore from "./redux/store";
 import { router } from "./router";
 import * as sessionActions from "./redux/session";
-import "./index.css";
 import { restoreCSRF, csrfFetch } from './redux/csrf';
+import "./normalize.css";
+import "./index.css";
 
 const store = configureStore();
 
