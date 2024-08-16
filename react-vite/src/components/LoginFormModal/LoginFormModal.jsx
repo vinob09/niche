7373577328834657d523dd91b5489d6a5f0963b7
Chanary Wilson <chanarykw@gmail.com>
2024-08-16 16:11:20 -0700

    Fixed error messages not populating on login

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -7,22 +7,22 @@ import "./LoginFormModal.css";
 function LoginFormModal() {
   const dispatch = useDispatch();
   const [user, setUser] = useState({ email: '', password: '' });
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({ email: '', password: '' });
   const [disableLogin, setDisableLogin] = useState(true);
   const { closeModal } = useModal();
 
   useEffect(() => {
-    if(user.email && user.password){
+    if (user.email && user.password) {
       setDisableLogin(false)
-    }else{
+    } else {
       setDisableLogin(true)
     }
   }, [user.email, user.password])
- 
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setErrors({});
+    setErrors({ email: '', password: '' }); 
     const serverResponse = await dispatch(
       thunkLogin((user))
     );
@@ -34,17 +34,24 @@ function LoginFormModal() {
     }
   };
 
-  const handleDemo = async(e) => {
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+
+    setErrors({ ...errors, [name]: '' })
+  }
+
+  const handleDemo = async (e) => {
     e.preventDefault();
-    try{
+    try {
       await dispatch(thunkLogin({ email: "demo@aa.io", password: "password" }));
       closeModal();
-    } catch(res){
+    } catch (res) {
       const data = await res.json();
-      if(data && data.errors){
+      if (data && data.errors) {
         setErrors(data.errors)
-      } else{
-        setErrors({email: 'Unsuccessful Demo Login'})
+      } else {
+        setErrors({ email: 'Unsuccessful Demo Login' })
       }
     }
   }
@@ -57,21 +64,24 @@ function LoginFormModal() {
           Email
           <input
             type="text"
+            name="email"
             value={user.email}
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            onChange={handleInputChange}
             required
           />
+          {errors.email && <p>{errors.email}</p>}
         </label>
         <label>
           Password
           <input
             type="password"
+            name="password"
             value={user.password}
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            onChange={handleInputChange}
             required
           />
+          {errors.password && <p>{errors.password}</p>}
         </label>
-        {errors.user && <p className='errors'>{errors}</p>}
         <button type="submit" className="pill-button" disabled={disableLogin}>Log In</button>
         <button type="button" className="pill-button" onClick={handleDemo}>Demo User</button>
       </form>
